80ea1dbe3435c6d01b6b02954a95839d9c1522ab
Xu Shanning <go52fen@mytum.de>
2026-05-04 20:35:46 +0200

    fix: restore correct test

--- tests/test_nutrition.py
+++ tests/test_nutrition.py
@@ -1,4 +1,4 @@
 from calorie_plate.nutrition import calc_kcal_per_weight
 
 def test_calc_kcal_per_weight():
-    assert calc_kcal_per_weight(100, 200) == 201
+    assert calc_kcal_per_weight(100, 200) == 200
